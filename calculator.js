fd7965be642037be77328345107795a0b0f008ea
MatiasStorm <48760011+MatiasStorm@users.noreply.github.com>
2019-08-13 17:49:50 +0200

    Added button events and made error preventions

--- calculator.js
+++ calculator.js
@@ -1,7 +1,8 @@
 class Calculator{
   constructor(inputId){
     this.input = $(inputId);
-    this.validOperators = /[\/*+\.\(\)-]$/
+    this.parentheces = 0;
+    this.lastChar = "";
   }
   
   val(){
@@ -9,16 +10,46 @@ class Calculator{
   }
 
   typeNum(val){
-    this.input.val(this.val() + val);
+    if(/[\/*+\(\)-]/.test(this.lastChar) || 
+       this.lastChar == "" || 
+       /\.[0-9]*$/.test(this.val()) || // Allow decimal numbers.
+       /[\/*+\(\)-]*[1-9][0-9]*$/.test(this.val())) { // Operators followed by one 1-9 and n 0-9.
+      this.input.val(this.val() + val);
+      this.lastChar = val;
+    }
   }
   
   typeOperator(op){
     var origVal = this.val();
-    if(this.validOperators.test(this.val().charAt(this.val().length - 1))){
-      this.input.val(origVal.substr(0, origVal.length - 1) + op);
+    if(op == "("){
+      if(/[\/*+-]$/.test(this.lastChar) || this.lastChar == ""){
+        this.parentheces += 1;
+        this.input.val(origVal + op);
+        this.lastChar = op;
+      }
     }
-    else if(origVal.length > 0){
-      this.input.val(this.val() + op);
+    else if(op == ")"){
+      if(/[0-9][/*+\.-]$/.test(this.lastChar) && this.parentheces > 0){
+        this.parentheces -= 1;
+        this.input.val(origVal.substr(0, origVal.length - 1) + op);
+        this.lastChar = op;
+      }
+      else if((/[0-9]/.test(this.lastChar) || this.lastChar == ")") && this.parentheces > 0){
+        this.parentheces -= 1;
+        this.input.val(this.val() + op);
+        this.lastChar = op;
+      }
+    }
+    
+    else if(this.lastChar != "("){
+      if(/[\/*+\.-]$/.test(this.lastChar)) {
+        this.input.val(origVal.substr(0, origVal.length - 1) + op);
+        this.lastChar = op;
+      }
+      else if(origVal.length > 0){
+        this.input.val(this.val() + op);
+        this.lastChar = op;
+      }
     }
   }
   
@@ -29,17 +60,29 @@ class Calculator{
   
   clear(){
     this.input.val("");
+    this.lastChar = "";
   }
   
   negate(){
     var origVal = this.val();
-    console.log(origVal)
     if(origVal.charAt(0) == "-") {
       this.input.val(origVal.substr(1, origVal.length - 1));
     }
     else{
       this.input.val("-" + origVal);
     }
+  }
+  
+  delete(){
+    var origVal = this.val()
+    this.input.val(origVal.substr(0, origVal.length - 1))
+    if(this.lastChar == "("){
+      this.parentheces -= 1
+    }
+    else if (this.lastChar == ")"){
+      this.parentheces += 1;
+    }
+    this.lastChar = origVal.charAt(origVal.length - 2)
   }  
 }
 
@@ -58,8 +101,14 @@ $("#minus").click(function(){c.typeOperator("-")});
 $("#times").click(function(){c.typeOperator("*")});
 $("#divide").click(function(){c.typeOperator("/")});
 $("#power").click(function(){c.typeOperator("^")});
+$("#power2").click(function(){c.typeOperator("^2")});
+$("#right_par").click(function(){c.typeOperator("(")});
+$("#left_par").click(function(){c.typeOperator(")")});
+
+
 $("#equal").click(function(){c.evalualte()});
 $("#clear").click(function(){c.clear()});
+$("#delete").click(function(){c.delete()});
 
 
 
