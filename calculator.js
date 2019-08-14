2ec7a1baa9823ac468f57d73dd3fb0157c130c1b
MatiasStorm <48760011+MatiasStorm@users.noreply.github.com>
2019-08-14 21:36:11 +0200

    Added restrictions to user input

--- calculator.js
+++ calculator.js
@@ -1,3 +1,7 @@
+/* TODO: 
+    - Check input for errors before evaluating.
+*/
+
 class Calculator{
   constructor(inputId){
     this.input = $(inputId);
@@ -61,35 +65,66 @@ class Calculator{
   clear(){
     this.input.val("");
     this.lastChar = "";
+    this.parentheces = 0;
   }
   
   negate(){
     var origVal = this.val();
-    if(/[0-9]/.test(this.lastChar)){
-      // if(/\(\-[0-9]*\)$/.test(this.val())){
-      // 
-      // }
-      // 
-      // 
-      if(origVal.charAt(0) == "-") {
-        this.input.val(origVal.substr(1, origVal.length - 1));
-      }
-      else{
-        this.input.val("-" + origVal);
-      }
+    var number, numberIndex, newVal;
+    if(/\(\-[0-9]*\.?[0-9]*\)$/.test(origVal)){
+      this.removeNegate();
+    }
+    else if(/[0-9]/.test(this.lastChar)){
+      this.addNegate();
     }
   }
   
+  removeNegate(){
+    var origVal = this.val();
+    var number, numberIndex, newVal;
+    for(var i = origVal.length - 1; i >= 0; i--){ 
+      if(origVal.charAt(i) == "-"){
+        numberIndex = i + 1;
+        break;
+      }
+    }
+    number = origVal.substring(numberIndex, origVal.length - 1);
+    newVal = origVal.substring(0, numberIndex - 2) + number;
+    this.input.val(newVal);  
+  }
+  
+  addNegate(){
+    var origVal = this.val();
+    var number, numberIndex, newVal;
+    for(var i = origVal.length - 1; i >= 0; i--){ 
+      if(( !/[0-9]/.test(origVal.charAt(i)) && origVal.charAt(i) != ".") || i == 0){
+        numberIndex = i == 0 ? i : i + 1;
+        break;
+      }
+    }
+    number = origVal.substring(numberIndex, origVal.length);
+    newVal = origVal.substring(0, numberIndex) + "(-" + number + ")";
+    this.input.val(newVal);
+  }
+  
+  
   delete(){
     var origVal = this.val()
-    this.input.val(origVal.substr(0, origVal.length - 1))
-    if(this.lastChar == "("){
-      this.parentheces -= 1
+    if(/\(\-[0-9]+\.*[0-9]*\)$/.test(origVal)){
+      this.removeNegate();
     }
-    else if (this.lastChar == ")"){
-      this.parentheces += 1;
+    else{
+      this.input.val(origVal.substr(0, origVal.length - 1))
+      if(this.lastChar == "("){
+        this.parentheces -= 1
+      }
+      else if (this.lastChar == ")"){
+        this.parentheces += 1;
+      }
     }
     this.lastChar = origVal.charAt(origVal.length - 2)
   }  
 }
+
+
 
